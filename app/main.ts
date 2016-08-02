037e369ef5a57f860d9db345e04cea39891347a4
Brandon Satrom <bsatrom@gmail.com>
2016-08-02 15:43:22 -0500

    added app images

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,8 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
+import {HTTP_PROVIDERS} from "@angular/http";
 import {AppComponent} from "./app.component";
+import {APP_ROUTER_PROVIDERS} from "./app.routes";
+import {setStatusBarColors} from "./utils/status-bar-util";
 
-nativeScriptBootstrap(AppComponent);
+setStatusBarColors();
+nativeScriptBootstrap(AppComponent, [HTTP_PROVIDERS, APP_ROUTER_PROVIDERS]);
---
+++ app/shared/config.ts
@@ -0,0 +1,5 @@
+export class Config {
+  static apiUrl = "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/";
+  static hubUrl = "https://http://10.171.5.102:1880/";
+  static token = "";
+}
